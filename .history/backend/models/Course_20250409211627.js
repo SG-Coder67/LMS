2bd80fd2a781d7cac000ba521063797ab39d9b9c
SG-Coder67 <sarathganesh0604@gmail.com>
2025-06-06 19:52:56 +0530

    deleted redundant files

--- .history/backend/models/Course_20250409211627.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const courseSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    createdAt: { type: Date, default: Date.now }
-});
-const Course = mongoose.model("Course", courseSchema);
-module.exports = Course;
